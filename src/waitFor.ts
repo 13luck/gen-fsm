29f3db413efa4341d284183da75846cdd8a6890b
aleksey.ozerov <i@13luck.ru>
2026-09-10 19:39:54 +0300

    Delete waitFor.ts

--- src/waitFor.ts
+++
@@ -1,8 +0,0 @@
-export async function* waitFor<T>(
-  expected: T
-): AsyncGenerator<unknown, T, unknown> {
-  while (true) {
-    const event = yield
-    if (event === expected) return event as T
-  }
-}
